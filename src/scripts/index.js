aeded1d8d74eb471ec0f90031acb36609f708b4b
Ulises Rafael Ramirez Martinez <vwalle.r.m@gmail.com>
2021-02-24 05:39:53 -0600

    finished

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,25 @@
 // import { qs, qsa, $on, $delegate } from './utils';
-
 import '../stylesheets/style.scss';
-
+// import html2pdf from 'html2pdf.js';
+import domtoimage from 'dom-to-image';
+import saveAs from 'file-saver';
+document.getElementById('downloader').addEventListener('click', () => {
+  const curriculum = document.getElementById('curriculum__wrapper');
+  domtoimage
+    .toBlob(curriculum, {
+      height: 1055,
+      width: 920,
+      style: {
+        position: 'relative',
+        margin: '0',
+        padding: '0',
+        right: '0',
+        left: '0',
+      },
+    })
+    .then((blob) => {
+      saveAs(blob, 'curriculum.png');
+    });
+  //html2pdf(curriculum).save();
+});
 // console.log('Hello!');
